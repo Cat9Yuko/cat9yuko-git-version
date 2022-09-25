94f3d3937735086cbf8806bf995093f4c6699e5b
cat9yuko <cat9mrchen@qq.com>
2022-09-26 00:21:35 +0800

    新增

---
+++ 网站/建设管理与质量安全中心/images/jazx-202209-index.js
@@ -0,0 +1,27 @@
+moment.locale("zh-cn");
+var time = moment().format("YYYY年MMM Do ");
+$(".localtime").text(time);
+$(function () {
+    var settings = {
+        trigger: 'hover',
+        // title: 'Pop Title',
+        // content: $(".nav-childs").html(),
+        width: 430,
+        // height: 32,
+        multi: false,
+        closeable: false,
+        style: 'webui-popover-mystyle',
+        padding: false,
+        placement: 'bottom'
+    };
+    var zxxgk = {content:$("#zxxgk .nav-childs").html(),width:325}
+    var xwdt = {content:$("#xwdt .nav-childs").html(),width:218}
+    var zcfg = {content:$("#zcfg .nav-childs").html()}
+    var gzhd = {content:$("#gzhd .nav-childs").html()}
+    var ztzl = {content:$("#ztzl .nav-childs").html(),width:325}
+    $('#zxxgk > a').webuiPopover('destroy').webuiPopover($.extend({}, settings, zxxgk));
+    $('#xwdt > a').webuiPopover('destroy').webuiPopover($.extend({}, settings, xwdt));
+    $('#zcfg > a').webuiPopover('destroy').webuiPopover($.extend({}, settings, zcfg));
+    $('#gzhd > a').webuiPopover('destroy').webuiPopover($.extend({}, settings, gzhd));
+    $('#ztzl > a').webuiPopover('destroy').webuiPopover($.extend({}, settings, ztzl));
+})
