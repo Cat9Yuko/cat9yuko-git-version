6dab91daf4a90a8ce9956b19989970c48ed32cf9
Cat9YuKo <cat9mrchen@qq.com>
2023-04-27 23:03:13 +0800

    更新

---
+++ vue/7第一个vue应用/vue.js
@@ -0,0 +1,13 @@
+Vue.createApp({
+    data() {
+        return {
+            title: 'Cat9',
+        }
+    },
+    // template: `
+    // <div>
+    //     第一个Vue应用——by
+    //     {{title}}
+    // </div>
+    // `,
+}).mount('#app');
