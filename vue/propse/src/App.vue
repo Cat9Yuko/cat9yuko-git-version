12d6505345d3dee2375d8b0b5b42c9ae47537ed6
Cat9YuKo <cat9mrchen@qq.com>
2023-05-08 20:19:33 +0800

    新增

---
+++ vue/propse/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <hd-button content="保存提交" type="success" style="margin-right: 10px;"/>
+    <hd-button content="删除" type="danger" style="margin-right: 10px;"/>
+    <hd-button content="反馈" type="info" style="margin-right: 10px;"/>
+    <hd-button content="禁用" :disabled="true" style="margin-right: 10px;"/>
+  </div>
+</template>
+
+<script>
+import hdButton from './components/Button.vue';
+  export default {
+    components: {hdButton},
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue/propse/src/components/Button.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="[type, { disabled }]" :style="$attrs.style">
+    {{ content }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      default: '确定'
+    },
+    type: {
+      type: String,
+      default: 'info',
+      validator(v) {
+        return ['success','danger','info'].includes(v)
+      }
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// yarn add -D sass
+div {
+  color: #ffffff;
+  padding: 5px 10px;
+  border-radius: 10px;
+  opacity: 0.6;
+  transition: 1s;
+  display: inline-block;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &.info {
+    background-color: #dddddd;
+  }
+
+  &.success {
+    background-color: #27ea60;
+  }
+
+  &.danger {
+    background-color: #e74c3c;
+  }
+
+  &.disabled {
+    opacity: 1;
+    background-color: #aaa !important;
+    cursor: not-allowed;
+  }
+}
+</style>
