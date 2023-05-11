7bd073353e68929615f51bf862f4fd39fd7dccd6
Cat9YuKo <cat9mrchen@qq.com>
2023-05-11 22:04:28 +0800

    新增

---
+++ vue/event/data/db.js
@@ -0,0 +1,5 @@
+export default [
+    {id: 1, title: 'html',preview:'/images/html.png'},
+    {id: 2, title: 'javascript',preview:'/images/js.png'},
+    {id: 3, title: 'electron',preview:'/images/electron.jpg'},
+]
---
+++ vue/event/src/components/Lesson.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <img :src="lesson.preview" alt="lesson.title">
+        <h3>{{ lesson.title }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['lesson']
+    }
+</script>
+
+<style lang="scss" scoped>
+ div {
+    border: 1px solid #dddddd;
+    text-align: center;
+    &:hover {
+        box-shadow: 0 0 20px #aaaaaa;
+    }
+    h3 {
+        padding: 0 0 20px 0;
+        margin: 0;
+    }
+    img {
+        width: 100%;
+    }
+ }
+</style>
