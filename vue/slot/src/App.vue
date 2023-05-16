aacb9e2a68b47575a884f6c96add1e64a099b016
Cat9YuKo <cat9mrchen@qq.com>
2023-05-16 23:11:18 +0800

    新增

---
+++ vue/slot/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <card/>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+export default {
+  components: { Card },
+  data() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// 安装sass yarn add -D sass
+div {}
+</style>
---
+++ vue/slot/src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Cat9Mrchen
+        <slot />
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
