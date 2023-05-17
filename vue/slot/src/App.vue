42c524540e8e3b6624b88a97bd3cf1ee389fcd33
Cat9YuKo <cat9mrchen@qq.com>
2023-05-17 22:42:06 +0800

    修改

--- vue/slot/src/App.vue
+++ vue/slot/src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-    <card/>
-  </div>
+    <card :content="content">
+    </card>
+    <button-def>aaa</button-def>
 </template>
 
 <script>
+import ButtonDef from './components/ButtonDef.vue';
 import Card from './components/Card.vue'
 export default {
-  components: { Card },
+  components: { Card, ButtonDef, },
   data() {
-
+    return {
+      content: '父组件的content'
+    }
+  },
+  methods: {
+    show(content) {
+      console.log('父组件' + content);
+    }
   }
 }
 </script>
--- vue/slot/src/components/Card.vue
+++ vue/slot/src/components/Card.vue
@@ -1,13 +1,23 @@
 <template>
     <div>
-        Cat9Mrchen
-        <slot />
+        <h1>Cat9Mrchen</h1>
+        <div style="color: red;" @click="show(content)">
+            
+            {{ content }}
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        props:{
+            content: {type: String, default: 'cat'}
+        },
+        methods: {
+            show(content) {
+                console.log(`子组件调用：${content}`);
+            }
+        }
     }
 </script>
 
