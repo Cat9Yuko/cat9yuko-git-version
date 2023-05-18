44d55183c25815d9c41b7479376d4d41108361e0
Cat9YuKo <cat9mrchen@qq.com>
2023-05-18 22:02:30 +0800

    新增

--- vue/slot/src/App.vue
+++ vue/slot/src/App.vue
@@ -1,7 +1,12 @@
 <template>
-    <card :content="content">
+    <!-- <card :content="content">
+      <button-def>aaa</button-def>
+    </card> -->
+    <card>
+      <template #header>Lorem ipsum,</template>
+      <template v-slot:main> dolor sit amet consectetur adipisicing elit. Qui debitis natus accusantium quibusdam commodi laboriosam maxime at reprehenderit magni molestias quam dolore ut autem omnis iste est iure</template>
+      <template v-slot:footer>, vel atque.</template>
     </card>
-    <button-def>aaa</button-def>
 </template>
 
 <script>
--- vue/slot/src/components/Card.vue
+++ vue/slot/src/components/Card.vue
@@ -1,26 +1,42 @@
 <template>
-    <div>
+    <!-- <div>
         <h1>Cat9Mrchen</h1>
         <div style="color: red;" @click="show(content)">
             
             {{ content }}
         </div>
+    </div> -->
+    <div>
+        <slot/>
+        <header><slot name="header"/></header>
+        <main><slot name="main"/></main>
+        <footer><slot name="footer"/></footer>
     </div>
 </template>
 
 <script>
-    export default {
-        props:{
-            content: {type: String, default: 'cat'}
-        },
-        methods: {
-            show(content) {
-                console.log(`子组件调用：${content}`);
-            }
+export default {
+    props: {
+        content: { type: String, default: 'cat' }
+    },
+    methods: {
+        show(content) {
+            console.log(`子组件调用：${content}`);
         }
     }
+}
 </script>
 
 <style lang="scss" scoped>
-
+div {
+    border: solid 1px #ddd;
+    header,main,footer {
+        padding: 10px;
+    }
+    main {
+        border-width: 1px 0 1px 0;
+        border-style: solid;
+        border-color: #ddd;
+    }
+}
 </style>
