8dbfa2d01ab9cef207296ec10f42040878ed956e
Cat9YuKo <cat9mrchen@qq.com>
2023-05-20 21:25:14 +0800

    新增

---
+++ vue/inject/src/components/XTextarea.vue
@@ -0,0 +1,41 @@
+<template>
+    <label>
+        <div>{{ title }}</div>
+        <textarea v-model="content" @input="$emit('update:modelValue',$event.target.value)"></textarea>
+    </label>
+</template>
+
+<script>
+export default {
+    props: ['title', 'modelValue'],
+    emits: ['update:modelValue'],
+    inject: ['webname'],
+    data() {
+        return {
+            content: this.modelValue
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+label {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+
+    div {
+        width: 100px;
+        color: #666;
+        font-size: 16px;
+        margin-right: 10px;
+    }
+
+    input {
+        border: 2px solid #ddd;
+        padding: 5px 10px;
+        color: #666;
+        outline: none;
+    }
+}
+</style>
