12d2a7b6a92ddc7ce81575078707b268d9912401
cat9yuko <cat9mrchen@qq.com>
2023-06-01 15:23:46 +0800

    新增

--- 网站/网部网站适老版/images_index/slbslb-202305-index.js
+++ 网站/网部网站适老版/images_index/slbslb-202305-index.js
@@ -4,3 +4,7 @@ $("#searchbox").click(function () {
     $('#searchword').show();
     $('#searchword').focus();
 });
+$(".chnltitle-container .chnltitle").hover(function(){
+    $(this).addClass("act").siblings().removeClass("act");
+    $(this).parent().next().find(".chnllist").eq($(this).index()).addClass("act").siblings().removeClass("act");
+});
