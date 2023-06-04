7482e5185530d880128aa1a5f033f3d6f93c3b0c
Cat9YuKo <cat9mrchen@qq.com>
2023-06-04 19:48:07 +0800

    新增

---
+++ vue/reacitive/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="change">{{ name }}</button>
+  </div>
+</template>
+
+<script>
+import { ref,reactive,toRefs } from 'vue'
+export default {
+  setup() {
+    const hd = reactive({name: 'Cat9YuKo'})
+    const change = () => hd.name = '我要饿死啦'
+    return { ...toRefs(hd), change }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
