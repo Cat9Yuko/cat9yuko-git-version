c71ebe2f6026331db28f3e27aea5ca545725fbdc
cat9yuko <cat9mrchen@qq.com>
2023-10-20 17:18:29 +0800

    新增修改

--- 网站/水利报社网站改版/images/slbs-202309-index.js
+++ 网站/水利报社网站改版/images/slbs-202309-index.js
@@ -1,3 +1,15 @@
 $(".navmenu").click(function(){
     $(".navmenu-childs").stop().slideToggle(".act")
 })
+// 查看电脑版
+// $('#godesktop').on('click', function () {
+//     var isShowPc = localStorage.setItem('isShowPc', true)
+//     $("meta[name='viewport']").attr("content", "width=1280, target-densitydpi=device-dpi")
+//     $('body').append('<div id="showMobi" class="showMobi"><img src="/images/trs_mobiIcon.png" ignoreapd="true" />返回手机版</div>')
+//   });
+//   // 返回手机版
+//   $('body').on('click', '#showMobi', function () {
+//     var isShowPc = localStorage.setItem('isShowPc', false)
+//     $("meta[name='viewport']").attr("content", "width=750, user-scalable=no, target-densitydpi=device-dpi")
+//     $('#showMobi').remove()
+//   });
