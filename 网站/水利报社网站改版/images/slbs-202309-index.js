503c872bf5e120c1f3f5dc8c9ac15ccf2b564d10
Cat9YuKo <cat9mrchen@qq.com>
2023-10-21 11:37:17 +0800

    修改

--- 网站/水利报社网站改版/images/slbs-202309-index.js
+++ 网站/水利报社网站改版/images/slbs-202309-index.js
@@ -2,14 +2,14 @@ $(".navmenu").click(function(){
     $(".navmenu-childs").stop().slideToggle(".act")
 })
 // 查看电脑版
-// $('#godesktop').on('click', function () {
-//     var isShowPc = localStorage.setItem('isShowPc', true)
-//     $("meta[name='viewport']").attr("content", "width=1280, target-densitydpi=device-dpi")
-//     $('body').append('<div id="showMobi" class="showMobi"><img src="/images/trs_mobiIcon.png" ignoreapd="true" />返回手机版</div>')
-//   });
-//   // 返回手机版
-//   $('body').on('click', '#showMobi', function () {
-//     var isShowPc = localStorage.setItem('isShowPc', false)
-//     $("meta[name='viewport']").attr("content", "width=750, user-scalable=no, target-densitydpi=device-dpi")
-//     $('#showMobi').remove()
-//   });
+$('#apptopc').on('click', function () {
+    // var isShowPc = localStorage.setItem('isShowPc', true)
+    $("meta[name='viewport']").attr("content", "width=1300, target-densitydpi=device-dpi")
+    $('body').append('<div id="backmobile" class="backmobile"><img src="/images/slbs-202310-mobileicon.png" ignoreapd="true" />返回手机版</div>')
+  });
+  // 返回手机版
+  $('body').on('click', '#backmobile', function () {
+    // var isShowPc = localStorage.setItem('isShowPc', false)
+    $("meta[name='viewport']").attr("content", "width=device-width")
+    $('#backmobile').remove()
+  });
