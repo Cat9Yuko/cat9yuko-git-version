afbc0aa94ec8708b4100c59a6318c013084f8949
cat9yuko <cat9mrchen@qq.com>
2023-11-10 18:01:29 +0800

    北信新增档案馆

--- 网站/北信档案馆/js/bxdag-202311-index.js
+++ 网站/北信档案馆/js/bxdag-202311-index.js
@@ -3,3 +3,12 @@ $(".nav-item").hover(function(){
 },function(){
     $(this).find(".nav-child").stop().slideUp();
 })
+
+ScrollReveal().reveal('.l1', {
+    delay: 150,
+    duration: 1000,
+    distance: '100px',
+    easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+    origin: 'left',
+    interval: 500
+    });
