21de45d2363f618e47f73b2d673b58c6c820a8a9
cat9yuko <cat9mrchen@qq.com>
2023-11-23 17:47:11 +0800

    新增修改

--- 前端项目练习/mycode/drawing-app/index.js
+++ 前端项目练习/mycode/drawing-app/index.js
@@ -1,41 +1,37 @@
-const canvas = document.getElementById('canvas');
-const increaseBtn = document.getElementById('increase');
-const decreaseBtn = document.getElementById('decrease');
-const sizeEL = document.getElementById('size');
-const colorEl = document.getElementById('color');
-const clearEl = document.getElementById('clear');
+const canvas = document.getElementById('canvas')
+const increaseBtn = document.getElementById('increase')
+const decreaseBtn = document.getElementById('decrease')
+const sizeEl = document.getElementById('size')
+const colorEl = document.getElementById('color')
+const clearEl = document.getElementById('clear')
 
 const ctx = canvas.getContext('2d');
 
 let size = 10
 let isPressed = false
-colorEl.value = 'black'
+colorEl.value = '#9122b9'
 let color = colorEl.value
 let x
 let y
 
 canvas.addEventListener('mousedown', (e) => {
     isPressed = true
-
     x = e.offsetX
     y = e.offsetY
 })
-
-canvas.addEventListener('mouseup', (e) => {
+canvas.addEventListener('mouseup', () => {
     isPressed = false
-
     x = undefined
     y = undefined
 })
 
 canvas.addEventListener('mousemove', (e) => {
-    if(isPressed) {
+    if (isPressed) {
         const x2 = e.offsetX
         const y2 = e.offsetY
 
         drawCircle(x2, y2)
         drawLine(x, y, x2, y2)
-
         x = x2
         y = y2
     }
@@ -50,37 +46,36 @@ function drawCircle(x, y) {
 
 function drawLine(x1, y1, x2, y2) {
     ctx.beginPath()
-    ctx.moveTo(x1, y1)
-    ctx.lineTo(x2, y2)
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
     ctx.strokeStyle = color
-    ctx.lineWidth = size * 2
+    ctx.lineWidth = size * 2;
     ctx.stroke()
 }
 
 function updateSizeOnScreen() {
-    sizeEL.innerText = size
+    sizeEl.innerHTML = size
 }
-
 increaseBtn.addEventListener('click', () => {
     size += 5
-
-    if(size > 50) {
+    if (size > 50) {
         size = 50
     }
-
     updateSizeOnScreen()
 })
-
 decreaseBtn.addEventListener('click', () => {
     size -= 5
-
-    if(size < 5) {
+    if (size < 5) {
         size = 5
     }
-
     updateSizeOnScreen()
 })
 
-colorEl.addEventListener('change', (e) => color = e.target.value)
+colorEl.addEventListener('change', (e) => {
+    color = e.target.value
+})
 
-clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height))
+clearEl.addEventListener('click', ()=> {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+})
