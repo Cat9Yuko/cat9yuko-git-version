ec68f61fcf2a234ae1326cf14574fc976fb2c9e7
cat9yuko <cat9mrchen@qq.com>
2023-12-22 09:05:57 +0800

    新增

--- 前端项目练习/mycode/drink-water/index.js
+++ 前端项目练习/mycode/drink-water/index.js
@@ -2,7 +2,6 @@ const smallCups = document.querySelectorAll('.cup-small')
 const listers = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
-
 updateBigCup()
 
 smallCups.forEach((cup, idx) => {
@@ -10,19 +9,17 @@ smallCups.forEach((cup, idx) => {
 })
 
 function highlightCups(idx) {
-    if (idx===7 && smallCups[idx].classList.contains("full")) idx--;
-    else if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
-        idx--
+    if (idx === 7 && smallCups[idx].classList.contains("full")) idx--;
+    else if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--;
     }
-
     smallCups.forEach((cup, idx2) => {
-        if(idx2 <= idx) {
+        if (idx2 <= idx) {
             cup.classList.add('full')
         } else {
             cup.classList.remove('full')
         }
     })
-
     updateBigCup()
 }
 
@@ -30,20 +27,20 @@ function updateBigCup() {
     const fullCups = document.querySelectorAll('.cup-small.full').length
     const totalCups = smallCups.length
 
-    if(fullCups === 0) {
+    if (fullCups === 0) {
         percentage.style.visibility = 'hidden'
         percentage.style.height = 0
     } else {
         percentage.style.visibility = 'visible'
         percentage.style.height = `${fullCups / totalCups * 330}px`
-        percentage.innerText = `${fullCups / totalCups * 100}%`
+        percentage.innerHTML = `${fullCups / totalCups * 100}%`
     }
 
-    if(fullCups === totalCups) {
+    if (fullCups === totalCups) {
         remained.style.visibility = 'hidden'
         remained.style.height = 0
     } else {
         remained.style.visibility = 'visible'
-        listers.innerText = `${2 - (250 * fullCups / 1000)}L`
+        listers.innerHTML = `${2-(250* fullCups / 1000)}L`
     }
 }
