94910220c4b8b3f4ff9398580c9707f57b0ae77e
cat9yuko <cat9mrchen@qq.com>
2024-01-26 16:53:49 +0800

    新增

--- 前端项目练习/JavaScript权威指南/chaper25/js/cookieutil.js
+++ 前端项目练习/JavaScript权威指南/chaper25/js/cookieutil.js
@@ -1,2 +1,41 @@
 // 因为在 JavaScript 中读写 cookie 不是很直观，所以可以通过辅助函数来简化相应的操作。与 cookie
 // 相关的基本操作有读、写和删除。这些在 CookieUtil 对象中表示如下：
+class CookieUtil {
+  static get(name) {
+    let cookieName = `${encodeURIComponent(name)}=`,
+      cookieStart = document.cookie.indexOf(cookieName),
+      cookieValue = null;
+
+    if (cookieStart > -1) {
+      let cookieEnd = document.cookie.indexOf(";", cookieStart);
+      if (cookieEnd == -1) {
+        cookieEnd = document.cookie.length;
+      }
+      cookieValue = decodeURIComponent(
+        document.cookie.substring(cookieStart + cookieName.length, cookieEnd)
+      );
+    }
+    return cookieValue;
+  }
+
+  static set(name, value, expires, path, domain, secure) {
+    let cookieText = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+    if (expires instanceof Date) {
+      cookieText += `; expires=${expires.toGMTString()}`;
+    }
+    if (path) {
+      cookieText += `; path=${path}`;
+    }
+    if (domain) {
+      cookieText += `; domain=${domain}`;
+    }
+    if (secure) {
+      cookieText += "; secure";
+    }
+    document.cookie = cookieText;
+  }
+
+  static unset(name, path, domain, secure) {
+    CookieUtil.set(name, "", new Date(0), path, domain, secure);
+  }
+}
--- 网站/水利报社网站改版/images/slbs-202309-index.js
+++ 网站/水利报社网站改版/images/slbs-202309-index.js
@@ -2,7 +2,10 @@ $(".navmenu").click(function(){
     $(".navmenu-childs").stop().slideToggle(".act")
     $(".bzzc-nav").stop().hide();
 })
-$(".bzzc").click(function(){
-  $(".bzzc-nav").stop().slideToggle("act");
-  $(".navmenu-childs").stop().hide();
-})
+// app部长之窗调整
+if($(window).width() <= 991) {
+  $(".bzzc").click(function(){
+    $(".bzzc-nav").stop().slideToggle("act");
+    $(".navmenu-childs").stop().hide();
+  })
+}
