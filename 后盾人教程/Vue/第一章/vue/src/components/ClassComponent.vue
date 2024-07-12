23d04edf55f60053ccf7462cb09674a077eaa6bd
cat9yuko <cat9mrchen@qq.com>
2024-07-12 11:26:02 +0800

    修改

---
+++ 后盾人教程/Vue/第一章/vue/src/components/ClassComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="c1" v-bind:class="{current: active}">后盾人</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                active: true
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .c1 {color: red;}
+</style>
