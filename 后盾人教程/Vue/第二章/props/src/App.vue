d6ae13a67cbf8402a6ec57159509f69ae68de03c
cat9yuko <cat9mrchen@qq.com>
2024-10-09 16:54:42 +0800

    新增

---
+++ 后盾人教程/Vue/第二章/props/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <hd-button :content="btContent" />
+  </div>
+</template>
+
+<script>
+import hdButton from './components/Button.vue'
+  export default {
+    components: {hdButton},
+    data() {
+      return {
+        btContent:'保存提交'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 后盾人教程/Vue/第二章/props/src/components/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>{{content}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      content: {
+        type: String,
+        default: '确定objk'
+      },
+      arr: {
+        type: Number,
+        default() {
+          return ['1,2',3]
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  div {
+    background-color: #27ae60;
+    color: #ffffff;
+    padding: 5px 10px;
+    border-radius: 10px;
+    opacity: 0.6;
+    transition: 1s;
+    display: inline-block;
+    cursor: pointer;
+    &:hover {
+      opacity: 1;
+    }
+  }
+</style>
