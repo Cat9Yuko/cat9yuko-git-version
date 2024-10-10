acf028e4fd345cb3c4164f4bf528d1d4208d15f1
cat9yuko <cat9mrchen@qq.com>
2024-10-10 16:59:24 +0800

    新增

--- 后盾人教程/Vue/第二章/props/src/components/Button.vue
+++ 后盾人教程/Vue/第二章/props/src/components/Button.vue
@@ -1,38 +1,65 @@
 <template>
-  <div>
-    <div>{{content}}</div>
-  </div>
+    <div :class="[type,{disabled}]">{{ content }}</div>
 </template>
 
 <script>
-  export default {
-    props: {
-      content: {
-        type: String,
-        default: '确定objk'
-      },
-      arr: {
-        type: Number,
-        default() {
-          return ['1,2',3]
-        }
+export default {
+  props: {
+    content: {
+      type: String,
+      default: '确定objk'
+    },
+    type: {
+      type: String,
+      default: 'info',
+      // 检测类型
+      validator(v) {
+        return ['success','danger','info'].includes(v)
       }
+    },
+    disabled: {
+      type: Boolean,
+      default: false
     }
+    // arr: {
+    //   type: Number,
+    //   default() {
+    //     return ['1,2', 3]
+    //   }
+    // }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  div {
-    background-color: #27ae60;
-    color: #ffffff;
-    padding: 5px 10px;
-    border-radius: 10px;
-    opacity: 0.6;
-    transition: 1s;
-    display: inline-block;
-    cursor: pointer;
-    &:hover {
-      opacity: 1;
-    }
+div {
+  color: #ffffff;
+  padding: 5px 10px;
+  border-radius: 10px;
+  opacity: 0.6;
+  transition: 1s;
+  display: inline-block;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 1;
   }
+
+  &.info {
+    background-color: #dddddd;
+  }
+
+  &.success {
+    background-color: #27ae60;
+  }
+  &.danger {
+    background-color: #e74c3c;
+  }
+  &.disabled {
+    background-color: #aaaaaa;
+    color: #666666;
+    opacity: 1;
+    cursor: default;
+  }
+}
 </style>
