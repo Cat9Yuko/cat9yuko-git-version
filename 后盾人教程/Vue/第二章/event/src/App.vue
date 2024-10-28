a0d7a6b5816195ea60b65b1be7f68af3be7ea423
cat9yuko <cat9mrchen@qq.com>
2024-10-28 16:55:17 +0800

    新增

---
+++ 后盾人教程/Vue/第二章/event/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <lesson v-for="item in db" :key="item.id" :lesson="item" />
+  </div>
+</template>
+
+<script>
+import db from '../data/db'
+import Lesson from './components/Lesson.vue';
+export default {
+  components: { Lesson },
+  data() {
+    return {
+      db: db
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 后盾人教程/Vue/第二章/event/src/components/Lesson.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img :src="lesson.preview" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['lesson']
+    }
+</script>
+
+<style lang="scss" scoped>
+    div {
+        border: 1px solid #ddd;
+        text-align: center;
+        
+    }
+</style>
