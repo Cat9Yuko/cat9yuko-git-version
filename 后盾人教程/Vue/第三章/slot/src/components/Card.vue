a29d83aa6bc9e17887df186d73c5914ea016b0a0
cat9yuko <cat9mrchen@qq.com>
2024-12-17 14:38:29 +0800

    新增第三章

---
+++ 后盾人教程/Vue/第三章/slot/src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>向军大叔</h2>
+    <div style="color: red;">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
