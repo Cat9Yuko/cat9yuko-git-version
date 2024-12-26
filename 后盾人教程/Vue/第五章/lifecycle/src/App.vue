48351f4171fab4aac6c7a2bf9641cd635681381b
cat9yuko <cat9mrchen@qq.com>
2024-12-26 16:54:05 +0800

    新增

--- 后盾人教程/Vue/第五章/lifecycle/src/App.vue
+++ 后盾人教程/Vue/第五章/lifecycle/src/App.vue
@@ -1,30 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div ref="el">
+    向军
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <child />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import Child from './components/Child.vue'
+export default {
+  components: { Child },
+  data() {
+    return {
+      hd: '向军大叔'
+    }
+  },
+  beforeCreate() {
+    console.log(`父组件 beforeCreate: ` + this.hd);
+  },
+  created() {
+    // 可以获取网络请求
+    console.log(`父组件 created: ` + this.hd);
+  },
+  beforeMount() {
+    // 挂载子组件
+    console.log(`父组件 beforeMount: ` + this.hd);
+    console.log( this.$refs.el);
+  },
+  mounted() {
+    // 挂载组件
+    console.log(`父组件 mounted: ` + this.hd);
+    console.log(this.$refs.el);
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 后盾人教程/Vue/第五章/lifecycle/src/components/Child.vue
@@ -0,0 +1,33 @@
+<template>
+  <div ref="el">
+    子组件
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hd: '向军大叔'
+    }
+  },
+  beforeCreate() {
+    console.log(`子组件 beforeCreate: ` + this.hd);
+  },
+  created() {
+    // 可以获取网络请求
+    console.log(`子组件 created: ` + this.hd);
+  },
+  beforeMount() {
+    console.log(`子组件 beforeMount: ` + this.hd);
+    console.log(`子组件 beforeMount: ` + this.$refs.el);
+  },
+  mounted() {
+    // 挂载组件
+    console.log(`子组件 mounted: ` + this.hd);
+    console.log(`子组件 mounted: ` + this.$refs.el);
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
