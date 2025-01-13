103d78ac60d666a47da95327e8319da1a01257e7
cat9yuko <cat9mrchen@qq.com>
2025-01-13 15:15:20 +0800

    新增

---
+++ 后盾人教程/Vue/第七章/script-setup/todo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ todos }}
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+const todos = ref([])
+onMounted(async () => {
+  todos.value = await fetch(`http://127.0.0.1:3002/news`).
+    then(r => r.json())
+})
+</script>
+
+<style lang="scss" scoped></style>
