70c03bde8df4655b02c3f273687330b96045d7e4
Cat9YuKo <cat9mrchen@qq.com>
2025-01-19 13:14:55 +0800

    修改

--- dst-mod/cat9-youko/modmain.lua
+++ dst-mod/cat9-youko/modmain.lua
@@ -1,3 +1,6 @@
+local MakePlayerCharacter = require "prefabs/player_common"
+
+-- 触手尖刺
 local use_count = GetModConfigData("use_count")
 AddPrefabPostInit("tentaclespike", function(inst)
     if not GLOBAL.TheWorld.ismastersim then return end
@@ -9,9 +12,26 @@ AddPrefabPostInit("tentaclespike", function(inst)
         end
     end
 end)
-
+-- 老奶奶
+-- 修改初始物品
+TUNING.GAMEMODE_STARTING_ITEMS.DEFAULT.WICKERBOTTOM = {"papyrus", "papyrus","spear_wathgrithr", "wathgrithrhat"}
+-- 修改可以睡觉和制作女武神物品
 AddPrefabPostInit("wickerbottom", function(inst)
     if not GLOBAL.TheWorld.ismastersim then return end
     inst:RemoveTag("insomniac")
     inst:AddTag("valkyrie")
 end)
+-- 女武神
+AddPrefabPostInit("wathgrithr", function(inst)
+    if not GLOBAL.TheWorld.ismastersim then return end
+    
+end)
+-- 打印日志
+print("=========================DEBUG=========================")
+for k,v in pairs(TUNING.GAMEMODE_STARTING_ITEMS.DEFAULT) do
+    print(k)
+    print(v)
+end
+-- print(GLOBAL.TheNet:getUserID())
+print(GLOBAL.TheNet:Say("我是菜逼！"))
+print("=========================DEBUG=========================")
