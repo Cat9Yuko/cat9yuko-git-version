6f27169aa39513383152bb6136c257c5671b0d99
cat9yuko <cat9mrchen@qq.com>
2025-01-23 14:32:07 +0800

    新增

--- 后盾人教程/Vue/第七章/script-setup/todo/src/components/Item.vue
+++ 后盾人教程/Vue/第七章/script-setup/todo/src/components/Item.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-    <input type="text">
+  <div class="item">
+    <input type="text" :value="todo.title">
+    <button @click="del">删除</button>
   </div>
 </template>
 
 <script setup>
-  defineProps({
-    
+const { todo } = defineProps({
+  todo: {
+    type: Object, required: true
+  }
+})
+const emit = defineEmits(['del'])
+const del = async () => {
+  await fetch(`http://127.0.0.1:3002/news/${todo.id}`, {
+    method: 'delete'
   })
+  emit('del')
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+div.item {
+  display: flex;
 
+  input {
+    flex: 1;
+    padding: 10px;
+  }
+}
 </style>
--- 后盾人教程/Vue/第七章/script-setup/todo/src/views/Todo.vue
+++ 后盾人教程/Vue/第七章/script-setup/todo/src/views/Todo.vue
@@ -1,23 +1,30 @@
 <template>
-    <div>
-        <item class="item" v-for="todo of todos" :key="todo.id" />
+    <div class="todo">
+        <item :todo="todo" @del="del" class="item" v-for="todo of todos" :key="todo.id" />
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import Item from '../components/Item.vue'
-const todos = ref([])
-todos.value = await fetch(`http://127.0.0.1:3002/news`).
-    then(r => r.json())
+    import { ref } from 'vue';
+    import Item from '../components/Item.vue'
+    
+    const todos = ref([])
+    todos.value = await fetch(`http://127.0.0.1:3002/news`).then(r => r.json())
+
+    const emit = defineEmits(['del'])
+
+    const del = async () => {
+        todos.value = await fetch(`http://127.0.0.1:3002/news`).then(r => r.json())
+    }
 </script>
 
-<style lang="scss" scoped>
-div {
+<style lang="scss">
+div.todo {
     display: flex;
     flex-direction: column;
+
     .item {
-        margin: 10px;
+        margin-bottom: 10px;
     }
 }
 </style>
