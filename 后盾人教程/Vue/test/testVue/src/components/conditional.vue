63069baefd87e7b5dbf3cefa55ac32473fa9058a
cat9yuko <cat9mrchen@qq.com>
2025-02-12 15:21:07 +0800

    新增

--- 后盾人教程/Vue/test/testVue/src/components/conditional.vue
+++ 后盾人教程/Vue/test/testVue/src/components/conditional.vue
@@ -17,6 +17,9 @@
             <p>Paragraph 2</p>
         </template>
     </div>
+    <div>
+        <h1 v-show="ok">Hello!</h1>
+    </div>
 </template>
 
 <script setup>
@@ -24,8 +27,12 @@ import { ref } from 'vue';
 // v-if
 const awesome = ref(true)
 const type = ref('B')
-const ok = ref(false)
+const ok = ref(true)
 // v-else-if
+
+// v-show
+// 不同之处在于 v-show 会在 DOM 渲染中保留该元素；v-show 仅切换了该元素上名为 display 的 CSS 属性。
+// v-show 不支持在 <template> 元素上使用，也不能和 v-else 搭配使用。
 </script>
 
 <style lang="scss" scoped></style>
